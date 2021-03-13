479bd3a686789f628248c7c70bbeba65bea97554
Cristian Enguidanos Nebot <cenguidanos@protonmail.com>
2021-03-13 21:26:43 +0100

    type(feat) finnish footer and layout

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { BaseLayoutComponent } from './common/components/layout/base-layout/base-layout.component'
 
 import { HomeComponent } from './home/home.component'
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  },
   { path: '**', redirectTo: '' }
 ]
 
